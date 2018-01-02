d42ef0f88b322403fefce560937654e492b1bce2
abraaocsantana <abraaocsantana@gmail.com>
2018-01-02 04:31:09 -0200

    Finally Dev'ing some functionallity

--- main.c
+++ main.c
@@ -3,8 +3,75 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <libgen.h>
+#include <string.h>
+
+#define MAX_PATHNODES_ON_MEM_PER_AREA 100
+
+#define FIRST_COL(maxCol) 0
+#define SECOND_COL(maxCol) maxCol/8
+#define THIRD_COL(maxCol) maxCol/2
+
+#define FIRST_SECOND_COL_SEPARATOR(maxCol) (maxCol/8) - 1
+#define SECOND_THIRD_COL_SEPARATOR(maxCol) (maxCol/2) - 1
 
 int maxRow,maxCol;
+int midCursor, leftCursor;
+
+struct PathNode{
+  char pathName[PATH_MAX];
+  enum NodeType{
+    Dir,
+    File,
+    Unknown
+  } type;
+};
+
+struct PathNodesPerArea{
+  int pathNodeCount;
+  struct PathNode pathNodes[MAX_PATHNODES_ON_MEM_PER_AREA];
+};
+
+struct PathNodesPerArea midPathNodes;
+struct PathNodesPerArea leftPathNodes;
+struct PathNodesPerArea rightPathNodes;
+
+char* lastPathName(struct PathNodesPerArea *nodeContainer){
+  return nodeContainer->pathNodes[nodeContainer->pathNodeCount].pathName;
+}
+
+enum NodeType* lastPathType(struct PathNodesPerArea *nodeContainer){
+  return &nodeContainer->pathNodes[nodeContainer->pathNodeCount].type;
+}
+
+void readPath(char pathName[], struct PathNodesPerArea *nodeContainer){
+  nodeContainer->pathNodeCount=0;
+  DIR		*d;
+	struct dirent	*dir;
+	d = opendir(pathName);
+  if (d)
+	{
+    seekdir(d,2);
+    while ((dir = readdir(d)) != NULL)
+		{
+      strcpy(lastPathName(nodeContainer),dir->d_name);
+      switch (dir->d_type) {
+        case DT_DIR:
+          (*lastPathType(nodeContainer)) = Dir;
+        break;
+        case DT_REG:
+          (*lastPathType(nodeContainer)) = File;
+        break;
+        default:
+          (*lastPathType(nodeContainer)) = Unknown;
+        break;
+      }
+      //printw("%i\n",nodeContainer->pathNodeCount);
+      //printw("%i\n",(*lastPathType(nodeContainer)));
+      //printw("%s",lastPathName(nodeContainer));
+      ++nodeContainer->pathNodeCount;
+    }
+  }
+}
 
 void drawSeparator(int col){
 	//init_pair(2,COLOR_RED,COLOR_BLACK);
@@ -15,55 +82,93 @@ void drawSeparator(int col){
 	//attroff(A_STANDOUT | COLOR_PAIR(2)/*| A_UNDERLINE*/);
 }
 
-void printDir(char path[], int colStart, int cursor){
-	DIR		*d;
-	struct dirent	*dir;
-	d = opendir(path);
-	int count = 0;
-	if (d)
-	{
-		init_pair(1,COLOR_BLUE,COLOR_BLACK);
-		seekdir(d,2);
-		while ((dir = readdir(d)) != NULL)
-		{
-			if(count == cursor){
-				attron(A_STANDOUT | COLOR_PAIR(1)/*| A_UNDERLINE*/);
-				mvprintw(count,colStart,"%s", dir->d_name);
-				attroff(A_STANDOUT | COLOR_PAIR(1)/*| A_UNDERLINE*/);
-			}else
-				mvprintw(count,colStart,"%s", dir->d_name);
-			++count;
-		}
-
-		closedir(d);
+void printDir(struct PathNodesPerArea* nodeContainer, int colStart, int cursor){
+  init_pair(1,COLOR_BLUE,COLOR_BLACK);
+  for (int i = 0; i < nodeContainer->pathNodeCount; ++i) {
+    /*switch (nodeContainer.pathNodes[i]) {
+      case Dir:
+      attron(A_STANDOUT | COLOR_PAIR(1));
+			mvprintw(i,colStart, dir->d_name);
+			attroff(A_STANDOUT | COLOR_PAIR(1));
+      break;
+      case File:
+      attron(A_STANDOUT | COLOR_PAIR(1));
+			mvprintw(i,colStart, dir->d_name);
+			attroff(A_STANDOUT | COLOR_PAIR(1));
+      break;
+    }*/
+    if(i == cursor){
+			attron(A_STANDOUT | COLOR_PAIR(1)/*| A_UNDERLINE*/);
+			mvprintw(i,colStart,/*"%s",*/ nodeContainer->pathNodes[i].pathName);
+			attroff(A_STANDOUT | COLOR_PAIR(1)/*| A_UNDERLINE*/);
+		}else{
+      mvprintw(i,colStart,/*"%s",*/ nodeContainer->pathNodes[i].pathName);
+    }
 	}
 }
 
+void draw(char *pathName){
+
+  char leftPathName[PATH_MAX];
+  strcpy(leftPathName,pathName);
+  strcat(leftPathName,"/..");
+
+  char righPathName[PATH_MAX];
+  strcpy(righPathName,pathName);
+  strcat(righPathName,"/");
+  strcat(righPathName,midPathNodes.pathNodes[midCursor].pathName);
+
+  readPath(leftPathName, &leftPathNodes);
+  readPath(pathName, &midPathNodes);
+
+  if(midPathNodes.pathNodes[midCursor].type == Dir){
+    readPath(righPathName, &rightPathNodes);
+  }else{
+    rightPathNodes.pathNodeCount=0;
+  }
+
+  printDir(&leftPathNodes, FIRST_COL(maxCol),leftCursor);
+
+	drawSeparator(FIRST_SECOND_COL_SEPARATOR(maxCol));
+
+	printDir(&midPathNodes, SECOND_COL(maxCol) ,midCursor);
+
+	drawSeparator(SECOND_THIRD_COL_SEPARATOR(maxCol));
+
+	printDir(&rightPathNodes, THIRD_COL(maxCol) ,-1);
+}
+
 int main(){
-	initscr();
-	start_color();
+  initscr();
 	raw();
+	keypad(stdscr, TRUE);
+	noecho();
 
 	getmaxyx(stdscr,maxRow,maxCol);
 
-	int cursor=0;
-	int leftCursor=0;
+  init_pair(1,COLOR_BLUE,COLOR_BLACK);
+  init_pair(2,COLOR_GREEN,COLOR_BLACK);
 
-	printDir("..", 0,leftCursor);
+	start_color();
 
-	drawSeparator(maxCol/8);
+  midCursor=0;
+  leftCursor=0;
 
-	printDir(".", maxCol/8 + 1 ,cursor);
+  char currentInput;
 
-	drawSeparator(maxCol/2);
+  do{
+    if(currentInput==3)
+      --midCursor;
+    if(currentInput==2)
+      ++midCursor;
+    draw(".");
+		refresh();
+    mvprintw(maxRow-1,0,"%i",midCursor);
+    mvprintw(maxRow-2,0,"%i",currentInput);
+    currentInput = getch();
+	}while(currentInput != 27);
 
-	printDir(".", maxCol/2 + 1 ,cursor);
-
-	char buf[PATH_MAX];
-	mvprintw(maxRow-1,10,"%s", realpath(".", buf));
-
-	while(getch() != 27){
-	}
 	endwin();
-	return (0);
+
+	return 0;
 }
